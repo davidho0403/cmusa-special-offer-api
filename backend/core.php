75f13641a1cfbd48e09bfe62e978dd3c298147ba
David Ho <davidho0403@gmail.com>
2022-05-19 00:38:47 +0800

    feat: initial json api processor

--- backend/core.php
+++ backend/core.php
@@ -5,3 +5,14 @@ require_once __DIR__ . "/../vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->load();
 
+require_once('model.php');
+$Model = new Model();
+
+$parameter = json_decode(file_get_contents('php://input'), true);
+
+$latitude = $parameter['latitude'];
+$longitude = $parameter['longitude'];
+
+$result = json_encode($topThree);
+header("Content-Type: application/json;");
+echo $result;

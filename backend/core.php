05d5938a8f001d4832c7cafeffb1d10a481a3d97
David Ho <davidho0403@gmail.com>
2022-05-19 00:41:54 +0800

    feat: calcDistance()
    此方法用以算出用戶與所有商家的距離資訊

--- backend/core.php
+++ backend/core.php
@@ -5,6 +5,41 @@ require_once __DIR__ . "/../vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->load();
 
+function calcDistance($store_list, $latitude, $longitude) {
+    $count = 0;
+    while (count($store_list) - $count >= 20) {
+        $api_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=';
+        for ($i = $count; $i < $count + 20; $i++) {
+            $api_url .= $store_list[$i]['store_address'];
+            if ($i < $count + 19) {
+                $api_url .= '|';
+            }
+        }
+        $api_url .=  '&origins=' . $latitude . '%2C' . $longitude . '&key=' . $_ENV['GOOGLE_API_KEY'];
+
+        $result = json_decode(file_get_contents($api_url), true);
+        for ($i = $count; $i < $count + 20; $i++) {
+            $store_list[$i]['distance'] = $result['rows'][0]['elements'][$i - $count]['distance']['value'];
+        }
+
+        $count += 20;
+    }
+    $api_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=';
+    for ($i = $count; $i < count($store_list); $i++) {
+        $api_url .= $store_list[$i]['store_address'];
+        if ($i < count($store_list) - 1) {
+            $api_url .= '|';
+        }
+    }
+    $api_url .=  '&origins=' . $latitude . '%2C' . $longitude . '&key=' . $_ENV['GOOGLE_API_KEY'];
+
+    $result = json_decode(file_get_contents($api_url), true);
+    for ($i = $count; $i < count($store_list); $i++) {
+        $store_list[$i]['distance'] = $result['rows'][0]['elements'][$i - $count]['distance']['value'];
+    }
+    return $store_list;
+}
+
 require_once('model.php');
 $Model = new Model();
 
